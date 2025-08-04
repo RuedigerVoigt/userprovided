97eeeb2ba2be5ae7f0715d37b66ab13c5e200d5a
Rüdiger Voigt <ruedigervoigt@gmx.net>
2025-08-04 23:33:24 +0200

    bugfix: Port 0 is a valid reserved port in TCP/UDP but was incorrectly excluded by the range check in is_port

--- userprovided/parameters.py
+++ userprovided/parameters.py
@@ -190,7 +190,7 @@ def is_port(port_number: int) -> bool:
     if not isinstance(port_number, int):
         raise ValueError('Port has to be an integer.')
 
-    if 0 < port_number < 65536:
+    if 0 <= port_number <= 65535:
         logging.debug('Port within range')
         return True
     logging.error('Port not within valid range from 0 to 65535')
