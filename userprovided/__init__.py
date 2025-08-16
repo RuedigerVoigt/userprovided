84a1ce089614a73444ebd2bcc573f225d1ef4b66
Rüdiger Voigt <ruedigervoigt@gmx.net>
2025-08-16 14:41:00 +0200

    update init

--- userprovided/__init__.py
+++ userprovided/__init__.py
@@ -5,12 +5,13 @@
 userprovided library
 ~~~~~~~~~~~~~~~~~~~~~
 Source: https://github.com/RuedigerVoigt/userprovided
-(c) 2020-2021 Rüdiger Voigt
+(c) 2020-2025 Rüdiger Voigt
 Released under the Apache License 2.0
 """
 
 from userprovided import _version
 from userprovided import date
+from userprovided import geo
 from userprovided import hashing
 from userprovided import mail
 from userprovided import parameters
